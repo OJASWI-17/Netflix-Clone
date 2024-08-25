2f20b8332cefd8878cf91a72bc0f6e6e329dac93
OJASWI-17 <157602711+OJASWI-17@users.noreply.github.com>
2024-08-25 19:42:50 +0530

    Update clone.js

--- clone.js
+++ clone.js
@@ -1,14 +1,20 @@
-function isMobileDevice() {
-    return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  }
+  document.addEventListener("DOMContentLoaded", function() {
+    function isMobileDevice() {
+      return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
 
-  if (isMobileDevice()) {
-     
-    document.body.innerHTML = "<h2>This site is currently only available for desktop users. Please visit from a computer.</h2>";
-    document.body.style.textAlign = "center";
-    document.body.style.marginTop = "20%";
-}
-
+    if (isMobileDevice()) {
+      // Display a message to mobile users
+      document.body.innerHTML = "<h2>This site is currently available only for desktop users. Please visit from a computer.</h2>";
+      document.body.style.display = "flex";
+      document.body.style.justifyContent = "center";
+      document.body.style.alignItems = "center";
+      document.body.style.height = "100vh";
+      document.body.style.textAlign = "center";
+      document.body.style.padding = "0 20px";
+      document.body.style.boxSizing = "border-box";
+    }
+  });
 let faqboxes = document.querySelectorAll('.faqbox');
 let drops = document.querySelectorAll('.drop');
  
