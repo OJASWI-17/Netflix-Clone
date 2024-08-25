6a953882b84af2ad3788c1756b23e85b76cc33de
OJASWI-17 <157602711+OJASWI-17@users.noreply.github.com>
2024-08-25 19:34:54 +0530

    Update clone.js

--- clone.js
+++ clone.js
@@ -1,3 +1,14 @@
+function isMobileDevice() {
+    return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+
+  if (isMobileDevice()) {
+     
+    document.body.innerHTML = "<h2>This site is currently only available for desktop users. Please visit from a computer.</h2>";
+    document.body.style.textAlign = "center";
+    document.body.style.marginTop = "20%";
+}
+
 let faqboxes = document.querySelectorAll('.faqbox');
 let drops = document.querySelectorAll('.drop');
  
